dd3be7410fdcdf17e22977c4eaed6fb5f1fd63b0
watama15 <watamasarunge@gmail.com>
2022-10-16 12:13:28 +0700

    new3

---
+++ plugins/downloader-igstalk.js
@@ -0,0 +1,30 @@
+import { instagramStalk } from '@bochilteam/scraper'
+
+let handler= async (m, { args, usedPrefix, command }) => {
+    if (!args[0]) throw `Example use ${usedPrefix}${command} <username>`
+    const {
+        username,
+        name,
+        description,
+        followersH,
+        followingH,
+        postsH,
+    } = await instagramStalk(args[0])
+    m.reply(`
+${name} *(${username})*
+https://instagram.com/${username.replace(/^@/, '')}
+*${followersH}* Followers
+*${followingH}* Following
+*${postsH}* Posts
+*Bio:* ${description}
+`.trim())
+}
+
+handler.help = ['igstalk'].map(v => v + ' <username>')
+handler.tags = ['downloader', 'premium']
+
+handler.command = /^(igstalk)$/i
+
+handler.premium = true
+
+export default handler
---
+++ plugins/hello.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+    let info = fs.readFileSync('./mp3/little-girl-sayingtalking-hello-sound-effectcute-sound-effectsadorable.opus')
+
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://www.youtube.com/channel/UClfUAjgtQ19vGnUbkQxTuAg",
+    mediaType: 2,
+    description: "https://www.youtube.com/channel/UClfUAjgtQ19vGnUbkQxTuAg", 
+    title: 'anyabot-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig  }}})
+}
+handler.customPrefix = /^(hallo|hello|alo|allo)$/i
+handler.command = new RegExp
+
+export default handler
